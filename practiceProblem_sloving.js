4a2d3b0d7f34a9bb64ff478163fc1ac62f6641db
TouhinTechnical <touhidulislam1090@gmail.com>
2022-02-02 17:42:30 +0600

    ...

--- practiceProblem_sloving.js
+++ practiceProblem_sloving.js
@@ -19,3 +19,53 @@ else{
         console.log('thirdNumber');
     }
 }
+
+// Exam Result
+var aliya = 95;
+var daliya = 66;
+var saliya = 80;
+var maliya = 59;
+var liliya = 47;
+var jaliya = 77;
+
+if(aliya <= 100){
+    console.log("Aliya got A+");
+}
+else{
+    console.log("Auto pass");
+}
+
+if(daliya >= 60 && daliya <= 70){
+    console.log("Daliya got C")
+}
+else{
+    console.log("Auto pass");
+}
+
+if(saliya >= 80 && saliya <= 90){
+    console.log("Saliya got A");
+}
+else{
+    console.log("Auto pass");
+}
+
+if(maliya >= 50 && maliya <= 60){
+    console.log("Maliya got D");
+}
+else{
+    console.log("Auto pass");
+}
+
+if(liliya >= 50 || liliya <= 50){
+    console.log("Liliya got F😑😥");
+}
+else{
+    console.log("Auto pass");
+}
+
+if(jaliya >= 70 && jaliya <= 80){
+    console.log("Jaliya got B");
+}
+else{
+    console.log("All is auto pass");
+}
