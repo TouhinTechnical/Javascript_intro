b3051b65d2bb9493c72996a62dac4397439dab3f
TouhinTechnical <touhidulislam1090@gmail.com>
2022-02-04 16:51:07 +0600

    object

---
+++ object.js
@@ -0,0 +1,21 @@
+var student1 = {
+    id: 115, 
+    name: "Jannatul Naima", 
+    marks: 91, 
+    phone: 023456745
+};
+// obejct value glo dker jonno
+console.log(student1);
+// object jakono value k dkte caile(variableName.propertyName)
+console.log(student1.name);
+// object kono kisu change korar jonno
+student1.marks = 81;
+student1["marks"] = 71;
+console.log(student1);
+
+var moblieDetails = {
+    color: 'black',
+    model: 'Iphone-7',
+    price: 15000,
+    Storage: '128gb'
+};
